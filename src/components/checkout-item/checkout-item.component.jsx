12d62cb6ddac99cfca0e24c0fdf488951faeeb20
nidarg <gradin_g@yahoo.com>
2021-02-26 20:15:32 +0200

    finish checkout page add functionality to increase/decrease products number

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { clearItemFromCart,removeItem,addItem } from '../../redux/cart/cart-actions';
 
 
 import './checkout-item.style.scss';
 
-const CheckoutItem = ({item:{imageUrl,name,price,quantity}})=>{
+const CheckoutItem = ({item, clearItemFromCart, removeItem,addItem })=>{
+    const{imageUrl,name,price,quantity} = item
     return(
         <div className='checkout-item'>
             <div className='image-container'>
                 <img alt='item' src={imageUrl}/>
             </div>
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick = {()=>removeItem(item)} >&#10096;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick = {()=>addItem(item)}>&#10097;</div> 
+            </span>
             <span className='price'>{price}</span>
-            <div className='remove-button'>&#10008;</div>
+            <div className='remove-button' onClick = {()=>clearItemFromCart (item)}>&#10008;</div>
 
         </div>
     )
 }
-export default CheckoutItem;
+export default connect(null,{clearItemFromCart,removeItem,addItem })(CheckoutItem);
