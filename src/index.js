9402df6285c588f50bdaabd1e521a20aa9352b5f
nidarg <gradin_g@yahoo.com>
2021-03-03 00:33:18 +0200

    add Collection Page

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import {store,persistor} from './redux/store';
 
 //Provider gives access to all redux features and
 // by including store in it -> access to states
 ReactDOM.render(
     <Provider store = {store}>
         <BrowserRouter>
-        <App />
+        <PersistGate persistor = {persistor}>
+            <App />
+        </PersistGate>
+       
         </BrowserRouter>
     </Provider>
 ,document.getElementById('root'));
